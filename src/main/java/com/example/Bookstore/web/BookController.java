4685b3fc3a432031256683f0df9a752041c20e5b
Maija Hoikka <hoikka.maija@gmail.com>
2019-03-06 11:48:48 +0200

    Adding spring security

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,12 @@ public class BookController {
 	
 	@Autowired
 	private CategoryRepository crepository;
+	
+	// Show all books
+    @RequestMapping(value="/login")
+    public String login() {	
+        return "login";
+    }
 	
 	//show all books
 	@RequestMapping(value="/booklist")
@@ -48,6 +55,7 @@ public class BookController {
 	
 	//delete book
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	@PreAuthorize("hasRole('ADMIN')")
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
 		return "redirect:../booklist";
